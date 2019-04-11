435193deb05a922f37f26132231467060bb7cf71
Zach Cossman <Zachcos@gmail.com>
2019-04-11 19:34:55 -0400

    Refactor index to class component and import GlobalStyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from 'react';
+import GlobalStyle from '../imports/globalStyle';
 
-export default () => <div>Hello world!</div>;
+export class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is a test</p>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
+
+export default Index;
