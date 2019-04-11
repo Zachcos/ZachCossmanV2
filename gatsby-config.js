ab3ce88f6271507d9b1a029e65aa9e17a733c384
Zach Cossman <Zachcos@gmail.com>
2019-04-11 19:52:52 -0400

    Install react-helmet dependencies and add to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: `${__dirname}/src/data`
       }
     },
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-transformer-json'
   ]
