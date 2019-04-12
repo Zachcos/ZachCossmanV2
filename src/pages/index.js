3e738466cf72d97302ce31b2e5c0c6151f6144f7
Zach Cossman <Zachcos@gmail.com>
2019-04-11 20:21:23 -0400

    Import and add Layout component to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import GlobalStyle from '../imports/globalStyle';
+import Layout from '../components/layout';
 
 export class Index extends React.Component {
   render() {
     return (
-      <div>
+      <Layout>
         <Helmet>
           <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet" />
@@ -13,7 +14,7 @@ export class Index extends React.Component {
         <p>This is a test</p>
         <h1>Does this work?</h1>
         <GlobalStyle />
-      </div>
+      </Layout>
     );
   }
 }
