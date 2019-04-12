a121cf66341eb4f50c209f18b7bb4965b6ab0119
Zach Cossman <Zachcos@gmail.com>
2019-04-11 20:29:20 -0400

    Add Logo styled component

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,4 +8,33 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-export default ({ children }) => <Container>{children}</Container>;
+const Logo = styled.h1`
+  font-size: 34px;
+  letter-spacing: 3px;
+  font-family: 'Varela Round', sans-serif;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-size: normal;
+  line-height: 1em;
+  width: 100%;
+	text-align: center;
+	display: inline-block;
+  margin: 0;
+  a {
+    text-decoration: none;
+    color: #222222;
+  }
+  a:hover {
+    color: #555555;
+  }
+}
+`;
+
+export default ({ children }) => (
+  <Container>
+    <Logo>
+      <a href="/">Zach Cossman</a>
+    </Logo>
+    {children}
+  </Container>
+);
