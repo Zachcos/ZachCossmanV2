742d065d36dc3c0ae86c3b8e47921f95dab296c4
Zach Cossman <Zachcos@gmail.com>
2019-04-11 20:36:44 -0400

    Remove old testing paragraphs

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ export class Index extends React.Component {
           <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet" />
         </Helmet>
-        <p>This is a test</p>
-        <h1>Does this work?</h1>
         <GlobalStyle />
       </Layout>
     );
