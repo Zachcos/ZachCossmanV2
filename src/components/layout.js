8137301c5571c9fee14970539ca6f72cbac54968
Zach Cossman <Zachcos@gmail.com>
2019-04-11 20:21:40 -0400

    Create Container styled component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1100px;
+  padding: 100px 100px 0;
+  border: 2px #dddddd solid;
+  margin: 0 auto;
+`;
 
 export default ({ children }) => (
-  <div>
+  <Container>
     This the the layout!
     {children}
-  </div>;
+  </Container>
 );
