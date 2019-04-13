c2597e0a2c04bb173f03f692c3e95fce6c055634
Zach Cossman <Zachcos@gmail.com>
2019-04-13 13:41:15 -0400

    Remove GlobalStyle component and React Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import GlobalStyle from '../imports/globalStyle';
 import Layout from '../components/layout';
 
@@ -7,10 +6,6 @@ export class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <Helmet>
-          <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet" />
-        </Helmet>
         <GlobalStyle />
       </Layout>
     );
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import GlobalStyle from '../imports/globalStyle';
 import Layout from '../components/layout';
 
 export default () => (
   <Layout>
     <p>This is the resume page!</p>
-    <GlobalStyle />
   </Layout>
 );
