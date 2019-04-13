1833f434cf25bb047a2b666a82629879b628c230
Zach Cossman <Zachcos@gmail.com>
2019-04-13 13:49:05 -0400

    Create TopNav styled component and nest entire nav inside it

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const TopNav = styled.div`
+  margin-top: 50px;
+  width: 100%;
+  text-align: center;
+  float: left;
+`;
 
 export default () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/bio">Bio</Link>
-    </li>
-    <li>
-      <Link to="/resume">Resume</Link>
-    </li>
-    <li>
-      <Link to="/media">Media</Link>
-    </li>
-    <li>
-      <Link to="/contact">Contact</Link>
-    </li>
-  </ul>
+  <TopNav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/bio">Bio</Link>
+      </li>
+      <li>
+        <Link to="/resume">Resume</Link>
+      </li>
+      <li>
+        <Link to="/media">Media</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+  </TopNav>
 );
