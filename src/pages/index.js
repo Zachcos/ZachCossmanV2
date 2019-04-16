9f4ce1e4a355e16cc8ee18186bdc5f373d290c33
Zach Cossman <Zachcos@gmail.com>
2019-04-15 23:38:33 -0400

    Add index styles and copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 import GlobalStyle from '../imports/globalStyle';
 import Layout from '../components/layout';
+
+const PageHeader = styled.h2`
+  font-family: 'Varela Round', sans-serif;
+  letter-spacing: 2px;
+  font-size: 22px;
+  font-weight: 400;
+  line-height: 1.2rem;
+  margin-bottom: 25px;
+  color: #333333;
+`;
+
+const Date = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  color: #000;
+  font-size: 14px;
+  font-style: italic;
+  margin: -20px 0 40px 25px;
+`;
 
 export class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <p>This is the home page</p>
+        <PageHeader>Back in NYC</PageHeader>
+        <Date>Monday, March 4th, 2019</Date>
+        <p>
+          Zach is back in NYC after playing <span className="emphasis">Jerry Allison</span> in{' '}
+          <span className="emphasis">BUDDY: The Buddy Holly Story</span> at Bucks County Playhouse in New Hope, PA.
+        </p>
+        &nbsp;
+        <p>
+          This production returned to the Bucks County Playhouse for the 3rd time, after wildly-successful runs in 2016
+          and 2017. Zach played Jerry Allison in both of the previous iterations.
+        </p>
+        &nbsp;
+        <p>
+          The production ran February 10th – March 3rd, with direction by Hunter Foster and choreography by Lorin
+          Latarro.
+        </p>
+        &nbsp;
+        <p>Stay tuned for information about upcoming gigs.</p>
         <GlobalStyle />
       </Layout>
     );
