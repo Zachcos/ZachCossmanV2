57e0b49dcd0dd35d1522ba4ed69b3b7de3020e3e
Zach Cossman <Zachcos@gmail.com>
2019-04-18 13:20:34 -0400

    Add resume page styles

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
+
+const DownloadLink = styled.a`
+  color: #777;
+  &: hover {
+    color: #e28161;
+  }
+`;
+
+const Resume = styled.div`
+  padding-top: 15px;
+  width: 100%;
+  vertical-align: middle;
+  text-align: center;
+  img {
+    max-width: 100%;
+  }
+`;
 
 export default () => (
   <Layout>
-    <p>This is the resume page!</p>
+    <DownloadLink href="http://www.zachcossman.com/wp-content/themes/ZCWP/images/ZachCossman.pdf">
+      Download Headshot / Resume
+    </DownloadLink>
+    <Resume>
+      <img src="http://www.zachcossman.com/wp-content/themes/ZCWP/images/resume.gif" alt="Zach Cossman Resume" />
+    </Resume>
   </Layout>
 );
