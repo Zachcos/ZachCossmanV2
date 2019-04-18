7b3faf6efcd3d506562091803501fa91b3bdf1ba
Zach Cossman <Zachcos@gmail.com>
2019-04-18 13:30:10 -0400

    Add contact page styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
+
+const PageHeader = styled.h2`
+  font-size: 22px;
+  letter-spacing: 2px;
+  font-family: 'Varela Round', sans-serif;
+  text-transform: none;
+  font-weight: 400;
+  font-size: normal;
+  line-height: 1.2em;
+  margin-bottom: 5px;
+  color: #333;
+  margin-bottom: 25px;
+`;
+
+const InfoContainer = styled.div`
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 30px;
+  a {
+    color: #777;
+    &:hover {
+      color: #e28161;
+    }
+  }
+`;
 
 export default () => (
   <Layout>
-    <p>This is the contact page!</p>
+    <PageHeader>Zach is represented by:</PageHeader>
+    <InfoContainer>
+      <img src="http://www.zachcossman.com/wp-content/themes/ZCWP/images/annsteeleagency.png" alt="Ann Steele Agency" />
+      <p>330 West 42nd St. 18th Floor. New York, NY 10036</p>
+      <p>p: 212.629.9112</p>
+      <a href="https://annsteeleagency.com" target="_blank">
+        www.annsteeleagency.com
+      </a>
+    </InfoContainer>
+    &nbsp;
+    <InfoContainer>
+      <p>For web design information visit:</p>
+      <a href="https://zachcossmandesigns.com" target="_blank">
+        www.ZachCossmanDesigns.com
+      </a>
+    </InfoContainer>
+    &nbsp;
+    <InfoContainer>
+      <p>For personal inquiries, email me at:</p>
+      <a href="mailto:Zachcos@gmail.com">Zachcos@gmail.com</a>
+    </InfoContainer>
+    &nbsp;
   </Layout>
 );
