72c5c437406f4920dbae3a1bc095efd74c342f9d
Zach Cossman <Zachcos@gmail.com>
2019-04-30 15:00:05 -0400

    Fix gatsby-config to reflect removal of gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/data`
-      }
-    },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-transformer-json'
-  ]
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components']
 };
