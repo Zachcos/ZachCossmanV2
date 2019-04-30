70a3be98ba1fc4578c8d90410dfb4ada4d8e6766
Zach Cossman <Zachcos@gmail.com>
2019-04-30 15:11:42 -0400

    Change path to local copy of resume pdf

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Layout from '../components/layout';
 
 import resume from '../../static/images/resume.gif';
+import resumePDF from '../../static/images/ZachCossman.pdf';
 
 const DownloadLink = styled.a`
   color: #777;
@@ -23,9 +24,7 @@ const Resume = styled.div`
 
 export default () => (
   <Layout>
-    <DownloadLink href="http://www.zachcossman.com/wp-content/themes/ZCWP/images/ZachCossman.pdf">
-      Download Headshot / Resume
-    </DownloadLink>
+    <DownloadLink href={resumePDF}>Download Headshot / Resume</DownloadLink>
     <Resume>
       <img src={resume} alt="Zach Cossman Resume" />
     </Resume>
